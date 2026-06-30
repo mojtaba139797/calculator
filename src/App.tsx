f8f3b75f6620cd9d279ccc13544fdda28c0734d9
mojtaba139797 <mojtabahabibi139797@gmail.com>
2026-06-30 23:34:14 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello Tailwind!
+      </h1>
+
+      <h4 className="text-2xl text-blue-300">hello</h4>
+    </>
+  );
+}
+
+export default App;
