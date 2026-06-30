182fcf407e26bc0fb136a9e4d3183feada118fb4
mojtaba139797 <mojtabahabibi139797@gmail.com>
2026-06-30 23:44:08 +0330

    initial commit dev

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello Tailwind!
-      </h1>
 
       <h4 className="text-2xl text-blue-300">hello</h4>
     </>
